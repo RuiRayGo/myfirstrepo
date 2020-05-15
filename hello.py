5cdc2a536f64f51064a676d43467a00749704465
ruim <ruim@ip-172-31-93-109.ec2.internal>
2020-05-15 18:39:35 +0000

    hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+#This is a comment
+#print("Hello, World!")
+
+name="Raj"
+age=22
+
+brothername="Vijay"
+brotherage=26
+
+agedifference=age-brotherage
+difference=abs(agedifference)
+
+print("My name is: ", name)
+print("My age is: ", age)
+print("My brother name is:", brothername)
+print("My borther age is: ",brotherage)
+print("We were born",difference, "years apart")
