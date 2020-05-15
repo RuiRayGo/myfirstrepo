037503b6a76d075e4fd50b86559b22263b5b9a7e
ruim <ruim@ip-172-31-93-109.ec2.internal>
2020-05-15 20:29:54 +0000

    first script

---
+++ rnaseqpipe.sh
@@ -0,0 +1,40 @@
+
+#Author Rui Ma
+#Description: this is a pipeline for primary rnaseq analysis
+
+#letting shell know where the programs are
+source /etc/profile.d/markcbm.sh
+
+#data prep
+cp -R /home/manager/linux Desktop/.
+cd ~/Desktop/linux/advanced/rnaseq/
+
+#Step 1 fastqc analysis
+fastqc fastq/*.fastq
+
+#Building index
+cd index
+bowtie-build mm9_chr1.fa mm9_chr1
+cd ..
+
+#Step 3 alignment
+tophat2  -G mm9_chr1.gtf -o  tophat_wt/  index/mm9_chr1 fastq/myoblast_wt.fastq
+
+#Step 4 check outputs
+ls
+ls tophat_wt/
+ls tophat_del/
+cat tophat_wt/align_summary.txt
+cat tophat_del/align_summary.txt
+
+#Step 5 create index
+samtools19 index tophat_wt/accepted_hits.bam
+samtools19 index tophat_del/accepted_hits.bam
+
+#Step 6 Differential Expression
+cuffdiff --no-update-check -o cuffdiff_out -L wt,del mm9_chr1.gtf tophat_wt/accepted_hits.bam tophat_del/accepted_hits.bam
+ls -lh cuffdiff_out
+
+echo "The RNA-Seq Pipeline is complete"
+
+
